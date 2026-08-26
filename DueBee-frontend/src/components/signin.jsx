169ec8d239bd69d2b5cef097e494d7e542c44432
Aoun Kazmi <aounabbaskazmi2005@gmail.com>
2026-08-26 21:02:32 +0500

    logo added

--- DueBee-frontend/src/components/signin.jsx
+++ DueBee-frontend/src/components/signin.jsx
@@ -39,6 +39,7 @@ function SignIn({ onAuthSuccess, onSwitchToSignUp }) {
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img src="/duebee.png" alt="DueBee" className="mx-auto h-10 w-auto" />
         <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
           Sign in to DueBee
         </h2>
