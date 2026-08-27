89391855cb04956e566b62c0eb7dd16cb40b9f0f
Aoun Kazmi <aounabbaskazmi2005@gmail.com>
2026-08-27 17:58:21 +0500

    ocr

--- DueBee-Backend/app/main.py
+++ DueBee-Backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
-from app.routes import bills, auth
+from app.routes import bills, auth, scan
 
 Base.metadata.create_all(bind=engine)
 
@@ -20,6 +20,7 @@ app.add_middleware(
 
 app.include_router(auth.router)
 app.include_router(bills.router)
+app.include_router(scan.router)
 
 
 @app.get("/")
