503b85da3892671c4db259c73cff7232d8bc5499
Aoun Kazmi <aounabbaskazmi2005@gmail.com>
2026-08-29 17:25:05 +0500

    adding

--- DueBee-Backend/app/main.py
+++ DueBee-Backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
-from app.routes import bills, auth
+from app.routes import bills, auth, analytics
 
 Base.metadata.create_all(bind=engine)
 
